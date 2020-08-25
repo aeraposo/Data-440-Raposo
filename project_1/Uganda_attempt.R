91f43f0396be7ae79b36e381a26e49975b129704
aeraposo <67920301+aeraposo@users.noreply.github.com>
2020-08-25 19:22:38 -0400

    final

---
+++ project_1/Uganda_attempt.R
@@ -0,0 +1,105 @@
+# Projecting, Plotting and Labelling Administrative Subdivisions
+# https://tyler-frazier.github.io/dsbook/describe.html#projecting-plotting-and-labelling-administrative-subdivisions
+
+# This package will be used to analyze and describe the various features in our data
+# the tidyverse is a collection of libraries- each of which works better on different types of data
+#install.packages("tidyverse", dependencies = TRUE)
+# Library() makes the package functions available for use here
+library(tidyverse)
+
+# install sf (simple features) package, works well with tidyverse libraries and will help us analyze spatial data
+#install.packages("sf", dependencies = TRUE)
+library(sf)
+
+# NOTE- ONE YOU RUN THE INSTALL PACKAGES LINES, YOU WON'T NEED THEM AGAIN (LIKE... EVER)
+# You'll only need to run the library('lib name you want') commands
+
+# Clear your workspace
+rm(list=ls(all=TRUE))
+
+# set your working directory
+setwd("/Users/aeraposo/Data-440-Raposo/project_1")
+
+# Remotely sensed vs. secondary data (satellite collected vs. reported data)
+# different file types we'll work with (.shp, .prj, .dbf)
+
+# create object that stores the spatial data from the portugal .shp file
+uga_int  <- read_sf("gadm36_UGA_shp/gadm36_UGA_0.shp")
+# show me some info about the geometry of the country
+st_geometry(uga_int)
+
+#ls()
+#View(lbr_int)
+#class(lbr_int)
+
+# use ggplot (a graphics library) to make a plot of the country
+library(ggplot2)
+# data = .shp file object, size = line size, alpha = transparency (between 0 and 1), color = 'boarder color', fill = 'fill color'
+ggplot() +
+  geom_sf(data = uga_int)
+
+
+ggplot() +
+  geom_sf(data = uga_int, size = 1.5, color = 'gold', fill = 'green', alpha = 0.5)
+
+# plot with title
+ggplot() +
+  geom_sf(data = uga_int,
+          size = 1.5,
+          color = "gold",
+          fill = "green",
+          alpha = 0.5) +
+  geom_sf_text(data = uga_int,
+               aes(label = NAME_0),
+               size = 10,
+               color = "black")
+
+# plot subcountries
+uga_adm1  <- read_sf("gadm36_UGA_shp/gadm36_UGA_1.shp")
+
+ggplot() +
+  geom_sf(data = uga_adm1,
+          size = 0.65,
+          color = "blue",
+          fill = "blue",
+          alpha = 0.65) +
+  geom_sf(data = uga_int,
+          size = 1.5,
+          color = "black",
+          fill = "black",
+          alpha = 0) +
+  geom_sf_text(data = uga_adm1,
+               aes(label = NAME_1),
+               size = 2,
+               color = "black") +
+  geom_sf_label(data = uga_int,
+                aes(label = NAME_0),
+                size = 12,
+                color = "black")
+
+# plot administrative subdivisions
+
+uga_adm2  <- read_sf("gadm36_UGA_shp/gadm36_UGA_3.shp")
+
+
+ggplot() +
+  geom_sf(data = uga_adm2,
+          size = 0.65,
+          color = "red",
+          fill = "pink",
+          alpha = 0.65) +
+  geom_sf(data = uga_adm1,
+          size = 0.65,
+          color = "gray50",
+          alpha = 0.65) +
+  geom_sf(data = uga_int,
+          size = 1.5,
+          alpha = 0) +
+  geom_sf_text(data = uga_adm2,
+               aes(label = NAME_3),
+               size = .1) +
+  geom_sf_text(data = uga_adm1,
+               aes(label = NAME_1),
+               size = .5)
+
+ggsave("uganda.png")
